f3339b269ff0c66ff387054a6e751cff7d2b6fd8
Ilias Papanikolaou <eliaspapanikolaou@hotmail.com>
2022-08-29 11:12:13 +0300

    manual acks mode

--- library-events-consumer/src/main/java/com/unipi/ipap/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/unipi/ipap/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,34 @@
 package com.unipi.ipap.libraryeventsconsumer.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+
+    /**
+     * This is Optional method:
+     * Use this only in case we want to change the default offset commit mode to manual (manual acks).
+     * <p>
+     * Taken from KafkaAnnotationDrivenConfiguration.class
+     */
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory.getIfAvailable());
+        // Set Ack mode to Manual
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        return factory;
+    }
 }
--- library-events-consumer/src/main/java/com/unipi/ipap/libraryeventsconsumer/consumer/LibraryEventsConsumerManualOffset.java
+++ library-events-consumer/src/main/java/com/unipi/ipap/libraryeventsconsumer/consumer/LibraryEventsConsumerManualOffset.java
@@ -3,18 +3,20 @@ package com.unipi.ipap.libraryeventsconsumer.consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.AcknowledgingMessageListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
-// @Component
+@Component
 @Slf4j
-public class LibraryEventsConsumer {
+public class LibraryEventsConsumerManualOffset implements AcknowledgingMessageListener<Long, String> {
 
     private static final String KAFKA_TOPIC = "library-events";
 
+    @Override
     @KafkaListener(topics = {KAFKA_TOPIC})
-    public void onMessage(ConsumerRecord<Long, String> consumerRecord) {
-
+    public void onMessage(ConsumerRecord<Long, String> consumerRecord, Acknowledgment acknowledgment) {
         log.info("ConsumerRecord: {}", consumerRecord);
-
+        acknowledgment.acknowledge();
     }
 }
