d9036087093b01a51ba3ad5dca42fa04c0d2f66a
Ilias Papanikolaou <eliaspapanikolaou@hotmail.com>
2022-08-29 11:15:44 +0300

    merge

--- library-events-consumer/src/main/java/com/unipi/ipap/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/unipi/ipap/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,13 +1,7 @@
 package com.unipi.ipap.libraryeventsconsumer.config;
 
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
